6e6b03c6a91efade666f4266ab9bfc6378201f7c
Miriã Evangelista <evangelista@furg.br>
2025-07-30 23:07:32 -0300

    modified

--- src/media_display/launch/display.launch.py
+++ src/media_display/launch/display.launch.py
@@ -1,19 +1,33 @@
 from launch import LaunchDescription
+from launch.actions import ExecuteProcess
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    ros_node_port = 9090  # Porta padrão do rosbridge
+
     return LaunchDescription([
+        # Inicia o rosbridge_server
         Node(
             package='rosbridge_server',
             executable='rosbridge_websocket',
             name='rosbridge_server',
+            output='screen',
+            parameters=[{'port': ros_node_port}]
+        ),
+
+        # Inicia o display_node
+        ExecuteProcess(
+            cmd=['python3', 'src/media_display/media_display/display_node.py'],
+            cwd='/home/mii/ros2_ws',
             output='screen'
         ),
-        Node(
-            package='media_display',
-            executable='display_node',
-            name='media_display_node',
-            output='screen',
-            prefix='python3'
+
+        # Abre automaticamente o Foxglove Studio no navegador
+        ExecuteProcess(
+            cmd=[
+                'xdg-open',
+                f'https://studio.foxglove.dev/?ds=rosbridge-websocket&ds.url=ws://localhost:{ros_node_port}'
+            ],
+            output='screen'
         )
     ])
--- src/media_display/setup.py
+++ src/media_display/setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 import os
 from glob import glob
 
@@ -7,20 +7,21 @@ package_name = 'media_display'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=['media_display'],
     data_files=[
         ('share/ament_index/resource_index/packages',
-         ['resource/' + package_name]),
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py'))
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='evangelista',
-    maintainer_email='evangelista@furg.br',
-    description='Um pacote para exibir diferentes tipos de mídia via ROS2.',
-    license='Apache-2.0',
+    maintainer='evangelista', 
+    maintainer_email='evangelista@furg.br', 
+    description='TODO: Package description',
+    license='TODO: License declaration',
     tests_require=['pytest'],
+    
     entry_points={
         'console_scripts': [
             'display_node = media_display.display_node:main',
